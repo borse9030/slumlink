794a25608f3421063b47376b0861ed264f648f2b
BHAVESH YT <bhaveshyt4321@gmail.com>
2025-07-31 08:33:55 +0000

    apiKey: "AIzaSyA7qjsSjNrknU7gN0KDXL2TfaxcKzPVfsY",   authDomain: "acing-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -14,11 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-    app = initializeApp(firebaseConfig);
-}
-
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
